0569a3bd75b0b091a770206e5543fe01cc1b693a
batzcocon <2504482620407@ingenieria.usac.edu.gt>
2025-05-14 22:34:51 -0600

    agregando base de datos

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ def actualizar_usuario(id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render provee el puerto como variable de entorno
+    init_db()
     app.run(host="0.0.0.0", port=port)
 
     
